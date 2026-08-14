62d5d02f4bfcd9ef2d24e858862d64ae2de690a8
maycon_v_f <maycon.fedechen@escola.pr.gov.br>
2026-08-14 15:19:43 +0000

    .

---
+++ objeto.js
@@ -0,0 +1,9 @@
+const listaCpfs= [1111,2222,3333];
+const imformacoesPessoa= [" nome", " jose", "idade", 32 , "cpf" ,"111122223333"];
+
+console.log (imformacoesPessoa[1]);
+
+const objetoPessoa={
+    idade: 32,
+    nome: "jose",
+};
