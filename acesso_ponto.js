d97d046579bcc07789c09db381514a83a8d5c1f5
maycon_v_f <maycon.fedechen@escola.pr.gov.br>
2026-08-21 14:28:42 +0000

    rtttt

---
+++ acesso_ponto.js
@@ -0,0 +1,14 @@
+const maycon={
+    nome: "maycon",
+     idade: 32,
+     cpf:"1122233345",
+     email:"andre@dominio.com",
+
+};
+
+console.log(
+` eu sou o maycon e venho te venho te ver hoje $(maycon.nome) e essa pessoa tem amizade comigo e o nome dela é  $(lucas  .idade) anos.`
+
+);
+
+console.log(`Os 3 primeiros digitos do cpf são $ ({ maycon. cpf. substring ( 0, 3)}`);
