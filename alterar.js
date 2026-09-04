94d685b8615e7ab1eb4cce7e4024a828508cb900
mayconfedechen-star <maycon.fedechen@escola.pr.gov.br>
2026-09-04 11:51:10 -0300

    .

--- alterar.js
+++ alterar.js
@@ -4,3 +4,15 @@
  };
 
  console.log( pessoa.nome);
+ console.log(pessoa.telefone);
+ pessoa.telefone = "11 222333444";
+ console.log( pessoa.telefone);
+ pessoa.nome = " Luma silva";
+ console.log(pessoa);
+ const novapessoa = {
+   nome:"pedro",
+ };
+
+ pessoa = novaPessoa;
+
+
---
+++ tipos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "joão",
+    idade: 24,
+    email: "joão@firma.com",
+    telefone: [ "1155555550 " ," 1144444440"],
+
+};
+
+ console.log( cliente.telefone);
